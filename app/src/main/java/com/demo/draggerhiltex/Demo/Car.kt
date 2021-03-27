a86dc610a8b690b49ad423336ac3f44fa82ed9d7
tech-deity <1316510155@kit.ac.in>
2021-03-27 18:39:19 +0530

    1.1

---
+++ app/src/main/java/com/demo/draggerhiltex/Demo/Car.kt
@@ -0,0 +1,15 @@
+package com.demo.draggerhiltex.Demo
+
+import android.util.Log
+import javax.inject.Inject
+
+class Car  {
+
+    @Inject
+    constructor()
+
+    fun getCar()
+    {
+        Log.d("main", "car is running like fuck ")
+    }
+}
